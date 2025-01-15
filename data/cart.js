5e969322f0d881628c743a5b142ed745221f78b4
Suneesh Nazar <suneesh.n@gmail.com>
2025-01-15 23:19:07 +0530

    Delete products from cart when clicking delete

--- data/cart.js
+++ data/cart.js
@@ -2,7 +2,7 @@
 Items added to the card will be captured using this js data structure
 */
 
-export const cart = [{
+export let cart = [{
   productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
   quantity: 2,
 }, {
@@ -28,4 +28,31 @@ export function addToCart(productId) {
       quantity: 1
     });
   }
+}
+
+export function removeFromCart(productId) {
+  // ===== Method 1 of removing the item from cart - Begin =====
+  // let deleteIndex;
+  // cart.forEach((cartItem, index) => {
+  //   if (productId === cartItem.productId) {
+  //     deleteIndex = index;
+  //   }
+  // });
+  // cart.splice(deleteIndex,1);
+  // ===== Method 1 of removing the item from cart - End =====
+
+  // ===== Method 2 of removing the item from cart - Begin =====
+  // const newCart = cart.filter(cartItem => productId !== cartItem.productId);
+  // cart = newCart;
+  // ===== Method 2 of removing the item from cart - Begin =====
+
+  const newCart = [];
+  
+  cart.forEach(cartItem => {
+    if (cartItem.productId !== productId) {
+      newCart.push(cartItem);
+    }
+  });
+
+  cart = newCart;
 }
