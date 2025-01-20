221d5866a6b4b20889c848db27df13addc8e7079
Suneesh Nazar <suneesh.n@gmail.com>
2025-01-20 18:17:39 +0530

    Handle missing quantity selector in addToCart function

--- data/cart.js
+++ data/cart.js
@@ -34,7 +34,7 @@ export function addToCart(productId) {
   });
 
   const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
-  const quantity = Number(quantitySelector.value);
+  const quantity = (quantitySelector) ? Number(quantitySelector.value) : 1;
   
   if (matchingItem) {
     matchingItem.quantity += quantity;
